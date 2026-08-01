6144c6f74a631195aa247d1a776e7969f9159abc
jeikudev <august.jakelourencevillar@gmail.com>
2026-08-01 22:28:56 +0800

    initial setup for user selection and cart

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        
+
+        Scanner userInputScanner = new Scanner(System.in);
 
         Menu[] products = {
                 new Menu("Pandesal", 5, 2),
@@ -14,19 +16,40 @@ public class Main {
 
         int productMinLength = 20;
         int priceAndQuantityMinLength = 10;
+        int productNumber = 1;
 
         System.out.println(
-                "____________________________________________\n" +
-                        "| " + Utils.spaceFiller("Name", productMinLength) +  Utils.spaceFiller("Price", priceAndQuantityMinLength) + Utils.spaceFiller("Quantity", priceAndQuantityMinLength) + "  |");
+                "_________________________________________________\n" +
+                        "| " + Utils.spaceFiller("Name", productMinLength + 4) +  Utils.spaceFiller("Price", priceAndQuantityMinLength) + Utils.spaceFiller("Quantity", priceAndQuantityMinLength) + "  |");
         for (Menu product : products) {
-            System.out.println("| " + Utils.spaceFiller(product.name, productMinLength)
+            System.out.println("| " + productNumber++ + " - " + Utils.spaceFiller(product.name, productMinLength)
                     + " "
                     + Utils.spaceFiller(Double.toString(product.price), priceAndQuantityMinLength)
                     + " "
                     + Utils.spaceFiller(Integer.toString(product.quantity), priceAndQuantityMinLength) + "|");
         }
-        System.out.println("---------------------------------------------");
+        System.out.print("-------------------------------------------------\n" +
+                "Select Order: ");
 
 
+        int userChoice = -1;
+
+        while (userChoice != 0) {
+            String input = userInputScanner.nextLine();
+            userChoice = Integer.parseInt(input);
+
+            if (userChoice < 0 || userChoice > products.length) {
+                System.out.print("Invalid choice, try again: ");
+                continue;
+            }
+
+            if (userChoice == 0) {
+                System.out.println("Proceeding to checkout...");
+            } else {
+                Menu chosenProduct = products[userChoice - 1];
+                System.out.println("You selected: " + chosenProduct.name);
+                System.out.print("Select Order: ");
+            }
+        }
     }
 }
